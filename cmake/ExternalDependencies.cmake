d7e2c01b7c533faa57b79a1b88153f139fc4ea42
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-04-14 17:39:41 +0200

    Use jeff-v0.2.0

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -5,7 +5,7 @@ if(BUILD_JEFF_MLIR_TRANSLATION)
     FetchContent_Declare(
         jeff
         GIT_REPOSITORY https://github.com/unitaryfoundation/jeff/
-        GIT_TAG e3d3cc8d19c78f2826cdf9d193be3c5a3b261333
+        GIT_TAG jeff-v0.2.0
     )
     list(APPEND FETCH_PACKAGES jeff)
 
